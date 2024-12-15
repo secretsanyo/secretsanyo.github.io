47d62589e8355e43ca9dd336be8b5ec9d2c5bfa3
secretsanyo <sanyosecret24@gmail.com>
2024-12-15 16:49:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,319 @@
+
+  const playlist = [
+  {
+    title: "Go Dig My Grave",
+    artist: "Lankum",
+    src: "LankumGoDigMyGrave.mp3",
+    art: "https://upload.wikimedia.org/wikipedia/en/8/8b/Lankum_-_False_Lankum.png",
+    description: "Recommended to me by an Irish music friend I met last summer, False Lankum is an album by irish trad-folk-with-a-modern-experimentalist-twist band Lankum. It was the first trad folk album to ever be named Guardian album of the year. The opening song, Go Dig My Grave, is a folk song dating back as early as 17th century, derived from the story also known as The Butcher Boy, or The Railroad Boy in the US--a young woman is abandoned by her lover, hangs herself, and is found by her father along with a suicide note with instructions for her burial. Lots of people--Clancy Brothers, Sinead Oconnor, Costello--have done renditions of this song, but none quite this unnerving. The Spotify version sounds a bit different from the Youtube one, so I encourage you to listen to that one too, or the whole album.",
+    videoClip: { id: "qhqpQiXnFx0", start: 10, end: 15 }
+  },
+    {
+    title: "Cruel Mother",
+    artist: "ØXN",
+    src: "CruelMother.mp3",
+    art: "https://www.clashmusic.com/wp-content/uploads/2023/09/OXN.jpg",
+    description: "From the same lead singer as Lankum, another sort of experimental modern Irish folk band, this time performing folk tale The Cruel Mother AKA Greenwood Sidey, and trying to bring attention to the persecution of women in the associated 9 minute short featuring Olwen Fouéré.",
+    videoClip: { id: "fuaLiowElxE", start: 192, end: 208 }
+  },
+  {
+    title: "Final Fantasy IV Theme",
+    artist: "Nobuo Uematsu & Máire Breatnach",
+    src: "FFIVCelticMoon.mp3",
+    art: "https://e.snmc.io/i/1200/s/9925f854c505f3280c9d31b572b363e8/1459718",
+    description: "In the mid-90s, Nintendo let composer Nuobo Uemasu rearrange the songs from Final Fantasy IV, in collaboration with folk Irish folk artist Máire Breatnach, to create the album Final Fantasy IV: Celtic Moon. This here is the theme song.",
+    videoClip: { id: "VRah-zYboFw", start: 24, end: 36 }
+  },
+    {
+    title: "Tegami",
+    artist: "Mei Semones",
+    src: "Tegami.mp3",
+    art: "https://f4.bcbits.com/img/a2751128368_16.jpg",
+    description: "Mei Semones brings bossa nova and jazzy tunes to pop, emo, and sometimes EDM, which I think is a really good example of a traditional Brazilian rhythm influencing global music.",
+    videoClip: { id: "9L1vvAJ1vnY", start: 0, end: 20 }
+  },
+    {
+    title: "Anmari Fukasugite (Far Too Deep)",
+    artist: "Tokedashita Garasubako",
+    src: "Tokedashita.mp3",
+    art: "https://www.jazzmessengers.com/54513-large_default/tokedashitagarasubako.jpg",
+    description: "So this is part of some genre I like called Japanese Acid Folk, and it's just a bunch of leftist Japanese students in the 70s being like Reject The West Embrace Experimentation With Traditional Japanese Tunes. I think this type of music is like loosely related to the late 60s Japanese student protests against Security cooperation treaties with the US.",
+    videoClip: { id: "PDOWrZfTfCI", start: 2, end: 22 }
+  }, 
+  {
+    title: "Ame No Asa (雨の朝)",
+    artist: "Abeichizoku",
+    src: "Abeichizoku.mp3",
+    art: "https://lightintheattic.net/cdn/shop/files/tmp_2F1693338129872-x29qkn6s618-749a7ad2a62200dc0bdfe0fea75a8fd3_2FFLR-003-Abeichizoku_1024x1024.png?v=1698117505",
+    description: "Founded in 1972 by members of a university folk club, this Japanese acid folk band recorded their sole album in 8 hours in what I can only assume was an existencial cry for help.",
+    videoClip: { id: "YzoUwQscAWU", start: 2, end: 10 }
+  }, 
+  {
+    title: "The Crane Wife 3",
+    artist: "The Decemberists",
+    src: "CraneWife3.mp3",
+    art: "https://upload.wikimedia.org/wikipedia/en/a/ae/Decemberists_TheCraneWife.jpg",
+    description: "Welcome to the things-that-people-call-folk-punk-but-I-am-not-really-sure-they-are-that section of the playlist. In honor of our mutual appreciation of that ill defined genre. I started with this absolute classic which you know all about, based off a Japanese folktale of the same name.",
+    videoClip: { id: "9S8IM6GQS9g", start: 120, end: 140 }
+  },  
+    {
+    title: "Cesta je zavřená",
+    artist: "Hledání",
+    src: "Cestajezavrena.mp3",
+    art: "https://f4.bcbits.com/img/a1345075055_16.jpg",
+    description: "Here is some Czech folk punk I found after going to Prague and becoming unhealthily obsessed with the Czech Republic.",
+    videoClip: { id: "DlC4ZVuBt9Y", start: 2, end: 12 }
+  }, 
+    {
+    title: "Este Infierno",
+    artist: "Bestiärio",
+    src: "BestiarioEsteInfierno.mp3",
+    art: "https://f4.bcbits.com/img/a1249486203_16.jpg",
+    description: "Here is a Colombian folk punk band I found one day when I was like hey I should listen to more music in Spanish. Are they good? Unclear. Do I listen to them anyways? Yeah! They are no longer active because a member of the band passed away. Now I feel bad about questioning their quality.",
+    videoClip: { id: "45x7tjCAC30", start: 0, end: 4 }
+  }, 
+  //   {
+  //   title: "Las Ánimas Serranas",
+  //   artist: "Keltika Hispanna",
+  //   src: "AnimasSerranas.mp3",
+  //   art: "https://f4.bcbits.com/img/a2165051208_16.jpg",
+  //   description: "Moving on to some modern takes on Spanish-language folk. In the album Leyendas de la Sierra de Guadarrama, Castilian band Keltika Hispanna narrate six legends from the folklore of the native tribes of the Sierra de Guadarrama in central Spain. I would call their specific genre dark folk or neofolk.",
+  //   videoClip: { id: "T2t_rKeCElk", start: 20, end: 30 }
+  // }, 
+    {
+    title: "Las Brujas de Miraflores",
+    artist: "Keltika Hispanna",
+    src: "BrujasMiraflores.mp3",
+    art: "https://f4.bcbits.com/img/a2165051208_16.jpg",
+    // description: "Ok wow I really didn't wanna have the same artist twice but I really like the instrumentals on this song even if I can't always vibe with the vocals. Also, I can never resist witch content.",
+      description: "Moving on to some modern takes on Spanish-language folk. In the album Leyendas de la Sierra de Guadarrama, Castilian band Keltika Hispanna narrate six legends from the folklore of the native tribes of the Sierra de Guadarrama in central Spain. I would call their specific genre dark folk or neofolk.",
+    videoClip: { id: "T2t_rKeCElk", start: 20, end: 30 }
+  },
+    {
+    title: "Hasta La Raiz",
+    artist: "Natalia Lafourcade",
+    src: "HastaLaRaiz.mp3",
+    art: "https://m.media-amazon.com/images/I/71O5qY00AfL._UF350,350_QL50_.jpg",
+    // description: "Ok wow I really didn't wanna have the same artist twice but I really like the instrumentals on this song even if I can't always vibe with the vocals. Also, I can never resist witch content.",
+      description: "Natalia Lafourcade is sort of the preeminent Mexican folk pop artist imo, perhaps together with Mon Laferte. There's some definitely traditional rhythms to her music, but it is decidedly pop. Her voice is wonderful and she's one of my favorite singers. Plus, she has the best name. ",
+    videoClip: { id: "T2t_rKeCElk", start: 20, end: 30 }
+  }, 
+    {
+    title: "House of the Rising Sun",
+    artist: "Alt-J fit Tuka",
+    src: "RisingSun.mp3",
+    art: "https://upload.wikimedia.org/wikipedia/en/7/75/Reduxer300x300.png",
+    // description: "Ok wow I really didn't wanna have the same artist twice but I really like the instrumentals on this song even if I can't always vibe with the vocals. Also, I can never resist witch content.",
+      description: "A lofi and remixed take on the classic American folk song House of the Rising Sun by one of my favorite bands. It's been said their self-remixing is bad. It's been said they should stop trying to insert rap into everything and just stick to their breathy vocals and breezeblocks. But I like their remix album and their take on House of the Rising Sun is one of my favorites so to hell with what is said!",
+    videoClip: { id: "XANPObswCsE", start: 50, end: 60 }
+  }, 
+    {
+    title: "House of the Rising Sun",
+    artist: "Joan Baez",
+    src: "RisingSunBaez.mp3",
+    art: "https://i1.sndcdn.com/artworks-000486300930-4lcv7p-t500x500.jpg",
+    // description: "Ok wow I really didn't wanna have the same artist twice but I really like the instrumentals on this song even if I can't always vibe with the vocals. Also, I can never resist witch content.",
+      description: "But my actual favorite is this one from the queen herself.",
+    videoClip: { id: "oVLyARq0n0I", start: 10, end: 20 }
+  }
+];
+
+const audioPlayer = document.getElementById('audio-player');
+const songTitle = document.getElementById('song-title');
+const songArtist = document.getElementById('song-artist');
+const songArt = document.getElementById('song-art');
+const playBtn = document.getElementById('play-btn');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const playlistContainer = document.getElementById('playlist');
+const progressBar = document.getElementById('progress-bar');
+const songDescription = document.getElementById('song-description');
+// const mainContent = document.getElementById('main-content');
+const landingPage = document.getElementById('landing-page');
+const enterButton = document.getElementById('enter-button');
+
+let currentSongIndex = 0;
+let isPlaying = false;
+let player;
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const firstPlaylistItem = playlistContainer.querySelector('li');
+//     if (firstPlaylistItem) {
+//         firstPlaylistItem.click();
+//     }
+// });
+
+
+function loadYouTubeAPI() {
+  const script = document.createElement('script');
+  script.src = "https://www.youtube.com/iframe_api";
+  document.body.appendChild(script);
+}
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-background', {
+        height: '100%',
+        width: '100%',
+        playerVars: {
+            autoplay: 1,
+            controls: 0,
+            showinfo: 0,
+            modestbranding: 1,
+            mute: 1,
+            rel: 0 // Disable related videos
+        },
+        events: {
+            onStateChange: handlePlayerStateChange
+        }
+    });
+}
+
+function loadVideo(videoId, startSeconds, endSeconds) {
+    player.loadVideoById({
+        videoId: videoId,
+        startSeconds: startSeconds,
+        endSeconds: endSeconds,
+        suggestedQuality: 'large'
+    });
+    player.mute();
+
+    // Set up a timer to loop the video segment
+    const loopInterval = (endSeconds - startSeconds) * 1000;
+    clearInterval(player.loopTimer); // Clear any existing loop timer
+    player.loopTimer = setInterval(() => {
+        player.seekTo(startSeconds, true);
+    }, loopInterval);
+}
+
+function handlePlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        // Reset video playback if it ends unexpectedly
+        const currentVideo = playlist[currentSongIndex].videoClip;
+        if (currentVideo) {
+            player.seekTo(currentVideo.start, true);
+        }
+    }
+}
+
+function renderPlaylist() {
+    playlistContainer.innerHTML = '';
+    playlist.forEach((song, index) => {
+        const li = document.createElement('li');
+        li.textContent = `${song.title} - ${song.artist}`;
+        li.dataset.index = index;
+        li.classList.toggle('active', index === currentSongIndex);
+        li.addEventListener('click', () => selectSong(index));
+        playlistContainer.appendChild(li);
+    });
+}
+
+function loadSong(index) {
+    const song = playlist[index];
+    songTitle.textContent = song.title;
+    songArtist.textContent = song.artist;
+    songArt.src = song.art;
+    songDescription.textContent = song.description;
+    audioPlayer.src = song.src;
+    updatePlaylistHighlight();
+
+    // Set the video background (update video ID and clip time)
+    const videoClip = song.videoClip;
+    if (videoClip) {
+        loadVideo(videoClip.id, videoClip.start, videoClip.end);
+    }
+}
+
+function updateProgressBar() {
+    const progress = (audioPlayer.currentTime / audioPlayer.duration) * 100;
+    progressBar.style.width = `${progress}%`;
+}
+
+audioPlayer.addEventListener('timeupdate', updateProgressBar);
+
+function updatePlaylistHighlight() {
+    document.querySelectorAll('.playlist li').forEach((li, index) => {
+        li.classList.toggle('active', index === currentSongIndex);
+    });
+    }
+
+// function playSong() {
+//   audioPlayer.play();
+//   isPlaying = true;
+//   playBtn.textContent = 'Pause';
+// }
+
+// function pauseSong() {
+//   audioPlayer.pause();
+//   isPlaying = false;
+//   playBtn.textContent = 'Play';
+// }
+function playSong() {
+  audioPlayer.play();
+  isPlaying = true;
+  document.getElementById('play-icon').style.display = 'none';
+  document.getElementById('pause-icon').style.display = 'block';
+}
+
+function pauseSong() {
+  audioPlayer.pause();
+  isPlaying = false;
+  document.getElementById('play-icon').style.display = 'block';
+  document.getElementById('pause-icon').style.display = 'none';
+}
+
+// function togglePlay() {
+//     const playIcon = document.getElementById('play-icon');
+//     const pauseIcon = document.getElementById('pause-icon');
+//     if (isPlaying) {
+//         pauseSong();
+//         playIcon.style.display = 'block';
+//         pauseIcon.style.display = 'none';
+//     } else {
+//         playSong();
+//         playIcon.style.display = 'none';
+//         pauseIcon.style.display = 'block';
+//     }
+// }
+function togglePlay() {
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+}
+
+
+function nextSong() {
+  currentSongIndex = (currentSongIndex + 1) % playlist.length;
+  loadSong(currentSongIndex);
+  playSong();
+}
+
+function prevSong() {
+  currentSongIndex = (currentSongIndex - 1 + playlist.length) % playlist.length;
+  loadSong(currentSongIndex);
+  playSong();
+}
+
+function selectSong(index) {
+  currentSongIndex = index;
+  loadSong(currentSongIndex);
+  playSong();
+}
+
+audioPlayer.addEventListener('ended', nextSong);
+
+playBtn.addEventListener('click', togglePlay);
+nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener('click', prevSong);
+
+// Render the playlist dynamically
+renderPlaylist();
+
+// Load the YouTube API
+loadYouTubeAPI();
+
+// Load the first song initially
+loadSong(currentSongIndex);
+
+
